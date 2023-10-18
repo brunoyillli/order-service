1880f098f890861faed4667a5c490f76d5b0e8a7
Bruno Mendes Carvalho <wolfteam1910@gmail.com>
2023-10-18 13:26:58 -0300

    criando consumer e producer

---
+++ src/main/java/io/github/brunoyillli/orderservice/core/consumer/EventConsumer.java
@@ -0,0 +1,27 @@
+package io.github.brunoyillli.orderservice.core.consumer;
+
+import io.github.brunoyillli.orderservice.core.document.Event;
+import io.github.brunoyillli.orderservice.core.utils.JsonUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class EventConsumer {
+
+    private final JsonUtil jsonUtil;
+
+    @KafkaListener(
+            groupId = "${spring.kafka.consumer.group-id}",
+            topics = "${spring.kafka.topic.notify-ending}"
+    )
+    public void consumerNotifyEndingEvent(String payload){
+        log.info("Receiving ending notification event {} from notify-ending topic", payload);
+        Event event = jsonUtil.toEvent(payload);
+        log.info(event.toString());
+
+    }
+}
